53e494e812c8b10cefa184e0e418c9745d04626b
Joshua Earnest <joshuaearnest@JPs-MacBook-Pro.fios-router.home>
2020-04-26 19:19:00 -0400

    Adding the Serverless API

---
+++ scripts/cognito-init.sh
@@ -0,0 +1,16 @@
+YOUR_COGNITO_REGION="us-east-1"
+YOUR_COGNITO_USER_POOL_ID="us-east-1_TSBlBspbj"
+YOUR_COGNITO_APP_CLIENT_ID="1chai72anfr4aj2etadmh97jjm"
+
+
+# aws cognito-idp sign-up \
+# --region $YOUR_COGNITO_REGION \
+# --client-id $YOUR_COGNITO_APP_CLIENT_ID \
+# --username admin@example.com \
+# --password Passw0rd!
+
+
+aws cognito-idp admin-confirm-sign-up \
+--region $YOUR_COGNITO_REGION \
+--user-pool-id $YOUR_COGNITO_USER_POOL_ID \
+--username admin@example.com
